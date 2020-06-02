a334701e9aed28e6b644504bca7f314f061bef47
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-06-02 12:30:06 +0900

    groupsテーブル、Groupモデル作成 #21

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_020143) do
+ActiveRecord::Schema.define(version: 2020_06_02_031454) do
+
+  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
