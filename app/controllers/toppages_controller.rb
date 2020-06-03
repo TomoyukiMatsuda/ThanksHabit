623e53542ec6f7afa6470808453679ff345218c3
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-06-04 06:53:39 +0900

    グループ一覧表示 #34

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,5 +2,8 @@ class ToppagesController < ApplicationController
   skip_before_action :login_required
   
   def index
+    if logged_in?
+      @groups = current_user.belong_groups.order(id: :desc).page(params[:page]).per(5)
+    end
   end
 end
