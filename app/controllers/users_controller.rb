ad74f28df8c2445bb385a14f4c85cd9bacc36114
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-06-06 06:43:40 +0900

    アカウント作成後ユーザ遷移先を修正 #39

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     if @user.save
       flash[:success] = 'ユーザを登録しました'
       session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザ登録に失敗しました'
       render :new
