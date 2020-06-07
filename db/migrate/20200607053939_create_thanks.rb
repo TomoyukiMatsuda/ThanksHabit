23368ecc84c21832cdf1612e9c1a72c3e563a816
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-06-07 17:46:50 +0900

    #63 を修正 thanksテーブル作成し直し #76

--- db/migrate/20200607053939_create_thanks.rb
+++ db/migrate/20200607053939_create_thanks.rb
@@ -3,7 +3,7 @@ class CreateThanks < ActiveRecord::Migration[5.2]
     create_table :thanks do |t|
       t.string :content, null: false
       t.references :user, foreign_key: true, null: false
-      t.references :receiver, foreign_key: { to_table: :users}, null: false
+      t.references :receiver, foreign_key: { to_table: :users }, null: false
       t.references :group, foreign_key: true, null: false
 
       t.timestamps
