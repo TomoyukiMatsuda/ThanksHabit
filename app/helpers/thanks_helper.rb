44a710cbb63c17b90c4a1aaade07756891093092
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-06-09 08:14:30 +0900

    同一ユーザへの感謝登録の動作改善 #84

--- app/helpers/thanks_helper.rb
+++ app/helpers/thanks_helper.rb
@@ -1,8 +1,8 @@
 module ThanksHelper
 
-  # userに対してその日のthankがあるかどうか確認
-  def thank_today(user, thanks)
-    thanks_to_receiver = thanks.where(receiver_id: user.id) # thankを受けたuserのthanksに絞り込み
+  # group内でuserに対してその日のthankがあるかどうか確認
+  def thank_today(thanks, user, group)
+    thanks_to_receiver = thanks.where(receiver_id: user.id, group_id: group.id) # 該当group内でthankを受けたuserのthanksに絞り込み
     thank_days = []
     thanks_to_receiver.each do |thank|
       thank_date = I18n.l thank.created_at
