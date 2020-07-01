4b7f8315674956b9fa18ba452aa091f0e53ab788
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-07-01 16:48:34 +0900

    Thank登録、削除の処理を効率化 #155

--- app/controllers/thanks_controller.rb
+++ app/controllers/thanks_controller.rb
@@ -4,7 +4,7 @@ class ThanksController < ApplicationController
   def create
     @thank = current_user.thanks.build(thank_params)
 
-    if @thank.tell_thank(current_user, thank_params[:receiver_id], thank_params[:group_id])
+    if @thank.save
       flash[:success] = "#{@thank.receiver.name} さんに感謝しました"
       redirect_to root_url
     else
@@ -16,9 +16,15 @@ class ThanksController < ApplicationController
   end
 
   def destroy
-    @thank.undo
-    flash[:warning] = '状態を元に戻しました'
-    redirect_to root_url
+    if @thank.destroy
+      flash[:warning] = '状態を元に戻しました'
+      redirect_to root_url
+    else
+      flash[:danger] = '状態を元に戻せませんでした'
+      @groups = current_user.groups.order(id: :desc).page(params[:page]).per(5)
+      @thanks = current_user.thanks.where.not(created_at: nil)
+      render 'toppages/index'
+    end
   end
 
   private
